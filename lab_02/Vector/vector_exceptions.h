f96cd874587b08f73a3adacc19e7e1c61a565a9e
Timofey <prostev_timofey@mail.ru>
2024-05-02 17:05:00 +0300

    Lab 02 добавлены исключения для вектора.

---
+++ lab_02/Vector/vector_exceptions.h
@@ -0,0 +1,38 @@
+#ifndef VECTOR_EXCEPTIONS_H
+#define VECTOR_EXCEPTIONS_H
+
+#include "exceptions.h"
+
+using exceptions::BaseException;
+
+namespace vector_exceptions {
+    class VectorBaseException : public BaseException {
+    public:
+        VectorBaseException() = default;
+        VectorBaseException(const char* filename, int line, const char* error_message = "Vector error.");
+    };
+
+    class VectorAllocationException : public VectorBaseException {
+    public:
+        VectorAllocationException(const char* filename, int line, const char* error_message = "Memory allocation error.");
+    };
+
+    class VectorDifferentSizeException : public VectorBaseException {
+    public:
+        VectorDifferentSizeException(const char* filename, int line, const char* error_message = "Vectors have different sizes. Operation is impossible.");
+    };
+
+
+    class VectorOutOfRangeException : public VectorBaseException {
+    public:
+        VectorOutOfRangeException(const char* filename, int line, const char* error_message = "Vector out of range error.");
+    };
+
+    class VectorUnableVectorMultiplicationException : public VectorBaseException {
+    public:
+        VectorUnableVectorMultiplicationException(const char *filename, int line, const char *error_message = "Vectors have different sizes. Operation is impossible.");
+    };
+}
+
+
+#endif //VECTOR_EXCEPTIONS_H
